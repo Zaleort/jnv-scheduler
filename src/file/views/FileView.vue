da3876a9dcf0689b6cb3688fb868773d8d47a19d
Alexandre Henrique Alves <zaleortgames@outlook.com>
2022-08-20 15:00:09 +0200

    Init

---
+++ src/file/views/FileView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <header>
+      <img src="" alt="">
+      <div></div>
+    </header>
+
+    <div>
+      <div></div>
+      <main></main>
+      <div></div>
+    </div>
+
+    <div></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import FileHeading from '../components/FileHeading.vue';
+import WeeklyWeight from '../components/WeeklyWeight.vue';
+import DietSchedule from '../components/DietSchedule.vue';
+import WorkoutSchedule from '../components/WorkoutSchedule.vue';
+import NutritionValues from '../components/NutritionValues.vue';
+</script>
