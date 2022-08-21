b006ee69342406d6537ebe69fd3a939696ca3d92
Alexandre Henrique Alves <zaleortgames@outlook.com>
2022-08-21 18:07:14 +0200

    Get necessary calories

--- src/file/components/NutritionValues.vue
+++ src/file/components/NutritionValues.vue
@@ -1,3 +1,27 @@
 <template>
-  <div></div>
+  <div class="nutrition-values">
+    
+  </div>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface NutritionValues {
+  proteins: number;
+  carbohydrates: number;
+  fat: number;
+}
+
+const props = withDefaults(defineProps<NutritionValues>(), {
+  proteins: 0,
+  carbohydrates: 0,
+  fat: 0,
+});
+
+const proteinCalories = computed(() => props.proteins * 4);
+const carbohydratesCalories = computed(() => props.carbohydrates * 4);
+const fatCalories = computed(() => props.fat * 9);
+
+const totalCalories = computed(() => proteinCalories.value + fatCalories.value + carbohydratesCalories.value);
+</script>
