dd45bfec4738f0bd5cca5708167b19361a1db8e8
Alexandre Henrique Alves <zaleortgames@outlook.com>
2022-08-21 18:06:58 +0200

    Define Customer structure

---
+++ src/file/components/CustomerData.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="customer-data">
+    <span>Nombre: {{ name }}</span>
+    <span>Móvil: {{ phone }}</span>
+    <span>Correo: {{ email }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface CustomerDataProps {
+  name: string;
+  phone: string;
+  email: string;
+}
+
+defineProps<CustomerDataProps>()
+</script>
--- src/file/components/WeeklyWeight.vue
+++ src/file/components/WeeklyWeight.vue
@@ -1,3 +1,15 @@
 <template>
-  <div></div>
+  <div class="weekly-weight">
+    <span v-for="(weight, i) of weights" :key="i">
+      {{ `S${i}: ${weight}kg` }}
+    </span>
+  </div>
 </template>
+
+<script setup lang="ts">
+interface WeeklyWeightProps {
+  weights: number[];
+}
+
+defineProps<WeeklyWeightProps>()
+</script>
