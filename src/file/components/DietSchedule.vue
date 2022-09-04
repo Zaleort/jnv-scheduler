8435d75cbd168492bc9be0e9eb2d48941f72ab73
Alexandre Henrique Alves <zaleortgames@outlook.com>
2022-09-04 20:03:35 +0200

    Table and observations in DietSchedule

--- src/file/components/DietSchedule.vue
+++ src/file/components/DietSchedule.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
-    
+  <div class="diet-schedule">
+    <h2 class="file__subheading">DIETA</h2>
+
+    <NutritionValues v-bind="nutritionValues" />
+    <Observations :observations="observations" />
   </div>
 </template>
+
+<script setup lang="ts">
+  import NutritionValues from './NutritionValues.vue';
+  import Observations from './Observations.vue';
+
+  interface DietScheduleProps {
+    observations?: string;
+    nutritionValues: {
+      proteins: number;
+      carbohydrates: number;
+      fat: number;
+    }
+  }
+
+  defineProps<DietScheduleProps>()
+</script>
