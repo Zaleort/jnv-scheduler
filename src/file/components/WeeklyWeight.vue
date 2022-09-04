9bb9cad5a79b6e2409c4521e5d3b3a2c0242c184
Alexandre Henrique Alves <zaleortgames@outlook.com>
2022-09-04 20:04:28 +0200

    WeeklyWeight styles

--- src/file/components/WeeklyWeight.vue
+++ src/file/components/WeeklyWeight.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="weekly-weight">
-    <span v-for="(weight, i) of weights" :key="i">
-      {{ `S${i}: ${weight}kg` }}
-    </span>
+  <div class="list-block">
+    <h2 class="list-block__title">Peso semanal</h2>
+    <ul class="list-block__list">
+      <li v-for="(weight, i) of weights" :key="i">
+        {{ `S${i}: ${weight}kg` }}
+      </li>
+    </ul>
   </div>
 </template>
 
