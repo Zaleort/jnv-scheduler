3ad6171f2d82ecf56d395572e0bf44229b4b480a
Alexandre Henrique Alves <zaleortgames@outlook.com>
2022-09-04 20:03:50 +0200

    FileHeading styles

--- src/file/components/FileHeading.vue
+++ src/file/components/FileHeading.vue
@@ -1,8 +1,15 @@
 <template>
   <header class="file-heading">
-    <img alt="JNV" src="" />
+    <div class="file-heading__logo">
+      <img class="file-heading__logo" alt="JNV" :src="logo" />
+    </div>
     <h1 class="file-heading__title">
-      Asesoría
+      ASESORÍAS
     </h1>
   </header>
 </template>
+
+<script setup lang=ts>
+  import logo from '@/assets/logo-cropped.png';
+</script>
+
