1661c4b30f9ab26d2f630dd4401f5113fb067a0b
Alexandre Henrique Alves <zaleortgames@outlook.com>
2022-09-04 20:04:16 +0200

    Observations styles

---
+++ src/file/components/Observations.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="observations">
+    <h2 class="observations__title">Observaciones</h2>
+    <div class="observations__content">
+      {{ observations }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  interface ObservationsProps {
+    observations?: string;
+  }
+
+  defineProps<ObservationsProps>();
+</script>
