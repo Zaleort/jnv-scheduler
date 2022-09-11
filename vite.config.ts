9c75cc1ba87c5faf3e8aee4aa4aedec0842571d9
Alexandre Henrique <zaleortgames@outlook.com>
2022-09-11 19:41:13 +0200

    Add singlefile plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 import { resolve } from 'path'; 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), viteSingleFile()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
